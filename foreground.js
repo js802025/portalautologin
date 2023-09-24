e55bf002bf39db14f3fbdac3e76daad299aef790
js802025 <30957566+js802025@users.noreply.github.com>
2023-09-24 18:34:38 -0500

    added delay

--- foreground.js
+++ foreground.js
@@ -1,9 +1,11 @@
 window.onload = () => {
+    setTimeout(() => {
     console.log("Injecting Auto-Login");
-    if (document.getElementsByClassName("spa-auth-row") != null) {
+    if (document.getElementsByClassName("spa-auth-row").length > 0) {
         console.log("Logging In");
         document.getElementsByClassName("spa-auth-row")[1].children[0].children[0].click()
     }
+}, 500)
 }
 
 
